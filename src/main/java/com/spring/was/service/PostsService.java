80ad018d33785faa54c2ff597cc2bb03edacc32c
kimdoyoung <gomydream89@gmail.com>
2022-08-15 01:57:25 +0900

    Google OAuth2 연동1

--- src/main/java/com/spring/was/service/PostsService.java
+++ src/main/java/com/spring/was/service/PostsService.java
@@ -1,10 +1,17 @@
 package com.spring.was.service;
 
+import com.spring.was.domain.posts.Posts;
 import com.spring.was.domain.posts.PostsRepository;
+import com.spring.was.web.dto.PostsListResponseDto;
+import com.spring.was.web.dto.PostsResponseDto;
 import com.spring.was.web.dto.PostsSaveRequestDto;
+import com.spring.was.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -14,5 +21,32 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게실글이 없습니다. id =" + id));
+
+        return new PostsResponseDto(posts);
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게실글이 없습니다. id =" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게실글이 없습니다. id =" + id));
+
+        postsRepository.delete(posts);
     }
 }
